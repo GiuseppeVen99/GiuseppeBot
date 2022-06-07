01ffe74667cb528d45594a30b4c8c9c95064b895
GiuseppeVen99 <101936290+GiuseppeVen99@users.noreply.github.com>
2022-06-07 13:27:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -95,7 +95,6 @@ client.on("messageCreate", message => {
             mode = mode ? mode == 2 ? "Repeat queue" : "Repeat song" : "Off";
             message.channel.send("Set repeat mode to `" + mode + "`");
         }
-    });
     
     const { RepeatMode } = require("distube");
     let mode;
@@ -111,103 +110,6 @@ client.on("messageCreate", message => {
             break;
     }
     message.channel.send("Set repeat mode to `" + mode + "`");
-
-    if (message.content == "!calqueue") {
-        const voiceChannel = message.member.voice.channel
-        if (!voiceChannel) {
-            return message.channel.send("Devi essere in un canale vocale")
-        }
-
-        const voiceChannelBot = message.guild.channels.cache.find(x => x.type == "GUILD_VOICE" && x.members.has(client.user.id))
-        if (voiceChannelBot && voiceChannel.id != voiceChannelBot.id) {
-            return message.channel.send("Qualun'altro sta già ascoltando della musica")
-        }
-
-        let queue = distube.getQueue(message)
-
-        if (!queue) return message.channel.send("Coda vuota")
-
-        let totPage = Math.ceil(queue.songs.length / 10)
-        let page = 1
-
-        let songsList = ""
-        for (let i = 10 * (page - 1); i < 10 * page; i++) {
-            if (queue.songs[i]) {
-                songsList += `${i + 1}. **${queue.songs[i].name.length <= 100 ? queue.songs[i].name : `${queue.songs[i].name.slice(0, 100)}...`}** - ${queue.songs[i].formattedDuration}\r`
-            }
-        }
-
-        let embed = new Discord.MessageEmbed()
-            .addField("Queue", songsList)
-            .setFooter({ text: `Page ${page}/${totPage}` })
-
-        let button1 = new Discord.MessageButton()
-            .setLabel("Indietro")
-            .setStyle("PRIMARY")
-            .setCustomId("indietro")
-
-        let button2 = new Discord.MessageButton()
-            .setLabel("Avanti")
-            .setStyle("PRIMARY")
-            .setCustomId("avanti")
-
-        if (page == 1) button1.setDisabled()
-        if (page == totPage) button2.setDisabled()
-
-        let row = new Discord.MessageActionRow()
-            .addComponents(button1)
-            .addComponents(button2)
-
-        message.channel.send({ embeds: [embed], components: [row] })
-            .then(msg => {
-                const collector = msg.createMessageComponentCollector()
-
-                collector.on("collect", i => {
-                    i.deferUpdate()
-
-                    if (i.user.id != message.author.id) return i.reply({ content: "Questo bottone non è tuo", ephemeral: true })
-
-                    if (i.customId == "indietro") {
-                        page--
-                        if (page < 1) page = 1
-                    }
-                    if (i.customId == "avanti") {
-                        page++
-                        if (page > totPage) page = totPage
-                    }
-
-                    let songsList = ""
-                    for (let i = 10 * (page - 1); i < 10 * page; i++) {
-                        if (queue.songs[i]) {
-                            songsList += `${i + 1}. **${queue.songs[i].name.length <= 100 ? queue.songs[i].name : `${queue.songs[i].name.slice(0, 100)}...`}** - ${queue.songs[i].formattedDuration}\r`
-                        }
-                    }
-
-                    let embed = new Discord.MessageEmbed()
-                        .addField("Queue", songsList)
-                        .setFooter({ text: `Page ${page}/${totPage}` })
-
-                    let button1 = new Discord.MessageButton()
-                        .setLabel("Indietro")
-                        .setStyle("PRIMARY")
-                        .setCustomId("indietro")
-
-                    let button2 = new Discord.MessageButton()
-                        .setLabel("Avanti")
-                        .setStyle("PRIMARY")
-                        .setCustomId("avanti")
-
-                    if (page == 1) button1.setDisabled()
-                    if (page == totPage) button2.setDisabled()
-
-                    let row = new Discord.MessageActionRow()
-                        .addComponents(button1)
-                        .addComponents(button2)
-
-                    msg.edit({ embeds: [embed], components: [row] })
-                })
-            })
-    }
 
     if (message.content == "!calskip") {
         const voiceChannel = message.member.voice.channel
