b9ddf01f3cf3c2debb7a40a404586f00555ff836
GiuseppeVen99 <101936290+GiuseppeVen99@users.noreply.github.com>
2022-06-07 14:02:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const distube = new DisTube(client, {
 })
 
 client.on("messageCreate", message => {
-    if (message.content.startsWith("!play")) {
+    if (message.content.startsWith("!calplay")) {
         const voiceChannel = message.member.voice.channel
         if (!voiceChannel) {
             return message.channel.send("Devi essere in un canale vocale")
@@ -48,7 +48,7 @@ client.on("messageCreate", message => {
         })
     }
 
-    if (message.content == "!pause") {
+    if (message.content == "!calpause") {
         const voiceChannel = message.member.voice.channel
         if (!voiceChannel) {
             return message.channel.send("Devi essere in un canale vocale")
@@ -69,7 +69,7 @@ client.on("messageCreate", message => {
         message.channel.send("Song paused")
     }
 
-    if (message.content == "!resume") {
+    if (message.content == "!calresume") {
         const voiceChannel = message.member.voice.channel
         if (!voiceChannel) {
             return message.channel.send("Devi essere in un canale vocale")
@@ -90,104 +90,7 @@ client.on("messageCreate", message => {
         message.channel.send("Canzone ripresa")
     }
 
-    if (message.content == "!queue") {
-        const voiceChannel = message.member.voice.channel
-        if (!voiceChannel) {
-            return message.channel.send("Devi essere in un canale vocale")
-        }
-
-        const voiceChannelBot = message.guild.channels.cache.find(x => x.type == "GUILD_VOICE" && x.members.has(client.user.id))
-        if (voiceChannelBot && voiceChannel.id != voiceChannelBot.id) {
-            return message.channel.send("Qualun'altro sta già ascoltando della musica")
-        }
-
-        let queue = distube.getQueue(message)
-
-        if (!queue) return message.channel.send("Coda vuota")
-
-        let totPage = Math.ceil(queue.songs.length / 10)
-        let page = 1
-
-        let songsList = ""
-        for (let i = 10 * (page - 1); i < 10 * page; i++) {
-            if (queue.songs[i]) {
-                songsList += `${i + 1}. **${queue.songs[i].name.length <= 100 ? queue.songs[i].name : `${queue.songs[i].name.slice(0, 100)}...`}** - ${queue.songs[i].formattedDuration}\r`
-            }
-        }
-
-        let embed = new Discord.MessageEmbed()
-            .addField("Queue", songsList)
-            .setFooter({ text: `Page ${page}/${totPage}` })
-
-        let button1 = new Discord.MessageButton()
-            .setLabel("Indietro")
-            .setStyle("PRIMARY")
-            .setCustomId("indietro")
-
-        let button2 = new Discord.MessageButton()
-            .setLabel("Avanti")
-            .setStyle("PRIMARY")
-            .setCustomId("avanti")
-
-        if (page == 1) button1.setDisabled()
-        if (page == totPage) button2.setDisabled()
-
-        let row = new Discord.MessageActionRow()
-            .addComponents(button1)
-            .addComponents(button2)
-
-        message.channel.send({ embeds: [embed], components: [row] })
-            .then(msg => {
-                const collector = msg.createMessageComponentCollector()
-
-                collector.on("collect", i => {
-                    i.deferUpdate()
-
-                    if (i.user.id != message.author.id) return i.reply({ content: "Questo bottone non è tuo", ephemeral: true })
-
-                    if (i.customId == "indietro") {
-                        page--
-                        if (page < 1) page = 1
-                    }
-                    if (i.customId == "avanti") {
-                        page++
-                        if (page > totPage) page = totPage
-                    }
-
-                    let songsList = ""
-                    for (let i = 10 * (page - 1); i < 10 * page; i++) {
-                        if (queue.songs[i]) {
-                            songsList += `${i + 1}. **${queue.songs[i].name.length <= 100 ? queue.songs[i].name : `${queue.songs[i].name.slice(0, 100)}...`}** - ${queue.songs[i].formattedDuration}\r`
-                        }
-                    }
-
-                    let embed = new Discord.MessageEmbed()
-                        .addField("Queue", songsList)
-                        .setFooter({ text: `Page ${page}/${totPage}` })
-
-                    let button1 = new Discord.MessageButton()
-                        .setLabel("Indietro")
-                        .setStyle("PRIMARY")
-                        .setCustomId("indietro")
-
-                    let button2 = new Discord.MessageButton()
-                        .setLabel("Avanti")
-                        .setStyle("PRIMARY")
-                        .setCustomId("avanti")
-
-                    if (page == 1) button1.setDisabled()
-                    if (page == totPage) button2.setDisabled()
-
-                    let row = new Discord.MessageActionRow()
-                        .addComponents(button1)
-                        .addComponents(button2)
-
-                    msg.edit({ embeds: [embed], components: [row] })
-                })
-            })
-    }
-
-    if (message.content == "!skip") {
+    if (message.content == "!calskip") {
         const voiceChannel = message.member.voice.channel
         if (!voiceChannel) {
             return message.channel.send("Devi essere in un canale vocale")
@@ -208,7 +111,7 @@ client.on("messageCreate", message => {
         message.channel.send("Song skipped")
     }
 
-    if (message.content == "!previous") {
+    if (message.content == "!calprevious") {
         const voiceChannel = message.member.voice.channel
         if (!voiceChannel) {
             return message.channel.send("Devi essere in un canale vocale")
@@ -229,7 +132,7 @@ client.on("messageCreate", message => {
         message.channel.send("Canzone precedente")
     }
 
-    if (message.content == "!stop") {
+    if (message.content == "!calstop") {
         const voiceChannel = message.member.voice.channel
         if (!voiceChannel) {
             return message.channel.send("Devi essere in un canale vocale")
