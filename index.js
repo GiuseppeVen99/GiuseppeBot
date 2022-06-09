f7b5c6133741dc9eb8b37e760ae06cacc712abf8
GiuseppeVen99 <101936290+GiuseppeVen99@users.noreply.github.com>
2022-06-09 13:36:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 // Code By GiulioPaesani (GiulioAndCode)
-client.user.setActivity("CalabriaCity", {
-    type: "STREAMING",
-    url: "mc.calabriacity.it"
-  });
-
 const Discord = require('discord.js');
 const client = new Discord.Client({
     intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"] //Aggiungere GUILD_VOICE_STATES
@@ -13,6 +8,11 @@ client.login(process.env.token);
 
 client.on("ready", () => {
     console.log("ONLINE");
+})
+
+client.user.setActivity("CalabriaCity", {
+    type: "STREAMING",
+    url: "mc.calabriacity.it"
 })
 
 const { DisTube } = require("distube")
